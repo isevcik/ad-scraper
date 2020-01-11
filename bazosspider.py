ece0b28142caea2191a36e0911abb4362b62d1c0
Ivan Sevcik <iv.sevcik@gmail.com>
2020-01-11 10:37:41 +0100

    made configurable

--- bazosspider.py
+++ bazosspider.py
@@ -6,20 +6,30 @@ class BazosSpider(scrapy.Spider):
     name = "bazos_spider"
     custom_settings = {
         'ITEM_PIPELINES': {
-            'scraper.JsonWriterPipeline': 300,
+            'bazosspider.JsonWriterPipeline': 300,
         }
     }
-    start_urls = [
-        "https://www.bazos.cz/search.php?hledat=keg&rubriky=www&hlokalita=60200&humkreis=25&cenaod=&cenado=&kitx=ano",
-        "https://www.bazos.cz/search.php?hledat=keg&rubriky=www&hlokalita=68605&humkreis=25&cenaod=&cenado=&kitx=ano",
-        "https://www.bazos.cz/search.php?hledat=pivni+sud&rubriky=www&hlokalita=60200&humkreis=25&cenaod=&cenado=&kitx=ano",
-        "https://www.bazos.cz/search.php?hledat=pivni+sud&rubriky=www&hlokalita=68605&humkreis=25&cenaod=&cenado=&kitx=ano"
-    ]
 
-    def parse(self, response):
+    # def __init__(self, *args, **kwargs):
+    #     self.definitions = {}
+    #     super(BazosSpider, self).__init__(*args, **kwargs)
+
+    def definitions_to_start_requests(self, definitions):
+        for query, params in definitions.items():
+            hledat = params["hledat"]
+            cenado = params["cenado"]
+            for hlokalita in params["hlokalita"]:
+                url = f"https://www.bazos.cz/search.php?hledat={hledat}&rubriky=www&hlokalita={hlokalita}&humkreis=25&cenaod=&cenado={cenado}&kitx=ano"
+                yield scrapy.Request(url=url, dont_filter=True, cb_kwargs={"query": query})
+
+    def start_requests(self):
+        return self.definitions_to_start_requests(self.definitions)
+
+    def parse(self, response, query):
         ITEM_SELECTOR = "table[class=inzeraty]"
         for item in response.css(ITEM_SELECTOR):
             yield {
+                "query": query,
                 "link": item.css("a::attr(href)").extract_first(),
                 "name": item.css(".nadpis").extract_first(),
                 "desc": item.css(".popis").extract_first()
@@ -42,4 +52,4 @@ class JsonWriterPipeline(object):
     def process_item(self, item, spider):
         if item["link"] not in self.items:
             item["time"] = time.time()
-        self.items[item["link"]] = item
+            self.items[item["link"]] = item
